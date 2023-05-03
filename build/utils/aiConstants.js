c95f27cba8ca18f81c58730ab7823a05421bf46a
harikabkr <harikabkr@gmail.com>
2023-05-02 23:55:50 -0400

    removed env variables from repo

--- build/utils/aiConstants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.chatGptModel = void 0;
-var chatGptModel = "gpt-3.5-turbo";
-exports.chatGptModel = chatGptModel;
--- build/utils/urls.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.OPENAI_CHAT_COMPLETION_URL = void 0;
-var OPENAI_CHAT_COMPLETION_URL = 'https://api.openai.com/v1/chat/completions';
-exports.OPENAI_CHAT_COMPLETION_URL = OPENAI_CHAT_COMPLETION_URL;
